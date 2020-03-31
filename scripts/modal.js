1bd33b52adc9d9e766ceebf85af93d548ccc2586
Scott Harvey <scottyharvey33@gmail.com>
2020-03-31 16:48:21 -0600

    master - correctingg bad js

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,37 +1,41 @@
 // Get the modal
 const modal = document.querySelector('#myModal');
 
-if (modal.classList.contains('is-active')) {
-  modal.classList.remove('invisible');
-} else {
-  modal.classList.add('invisible');
-}
+if (modal) {
 
-// If user clicks the button, open the modal
-showModal = () => {
-  modal.classList.remove('invisible');
-};
-
-// Grab elements that act as triggers to open the modal
-const triggers = document.querySelectorAll('[data-trigger="internet"]');
-
-triggers.forEach( trigger => {
-  trigger.addEventListener('click', () => showModal());
-})
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-displayNone = () => {
-  modal.classList.add('invisible');
-};
-
-// If user clicks on <span> (x), close the modal
-span.onclick = displayNone;
-
-// If user clicks anywhere outside of the modal, close it
-window.onclick = event => {
-  if (event.target == modal) {
+  if (modal.classList.contains('is-active')) {
+    modal.classList.remove('invisible');
+  } else {
     modal.classList.add('invisible');
   }
+
+  // If user clicks the button, open the modal
+  showModal = () => {
+    modal.classList.remove('invisible');
+  };
+
+  // Grab elements that act as triggers to open the modal
+  const triggers = document.querySelectorAll('[data-trigger="internet"]');
+
+  triggers.forEach( trigger => {
+    trigger.addEventListener('click', () => showModal());
+  })
+
+  // Get the <span> element that closes the modal
+  const span = document.getElementsByClassName("close")[0];
+
+  displayNone = () => {
+    modal.classList.add('invisible');
+  };
+
+  // If user clicks on <span> (x), close the modal
+  span.onclick = displayNone;
+
+  // If user clicks anywhere outside of the modal, close it
+  window.onclick = event => {
+    if (event.target == modal) {
+      modal.classList.add('invisible');
+    }
+  }
+
 }
