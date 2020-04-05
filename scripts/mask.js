39b628f42ffc780a4b77a29d2efc0c1813936d6b
Scott Harvey <scottyharvey33@gmail.com>
2020-04-05 12:59:30 -0600

    master - input mask update
    * Date of birth

--- scripts/mask.js
+++ scripts/mask.js
@@ -7,22 +7,7 @@ $(() => {
   $('.pin').mask('0000');
   $('#phone').mask('(000)000-0000');
   $('#SSN').mask('000-00-0000');
-  $('#birthday').mask('TN/ON/YNNN', {
-    translation: {
-      'T': {
-        pattern: /[0-3]/, optional: false
-      },
-      'N': {
-        pattern: /[0-9]/, optional: false
-      },
-      'O': {
-        pattern: /[0-1]/, optional: false
-      },
-      'Y': {
-        pattern: /[1-2]/, optional: false
-      }
-    }
-  });
+  $('#birthday').mask('00/00/0000');
   $('.credit-card-number').mask('0000 0000 0000 0000');
   $('.expire').mask('00/00');
   $('.cvv').mask('000');
