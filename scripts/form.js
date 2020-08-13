b14df7418a3b95cf8f6cad96d84aa3f00283c1a4
Scott Harvey <scottyharvey33@gmail.com>
2020-08-13 12:56:28 -0600

    master - update 5a js to include ssn and bday mask

--- scripts/form.js
+++ scripts/form.js
@@ -55,6 +55,84 @@ idOptions.on('change', (e) => {
 
 });
 
+$('#SSN').val("");
+$('#birthday').val("");
+
+function ssnMask() {
+  $('#SSN').addClass('hidden');
+  var maskVal = '';
+
+  for (var i = 0; i < $('#SSN').val().length; i++) {
+    if (i === 3 || i === 6) {
+      maskVal += '-';
+    } else {
+      maskVal += '*';
+    }
+  }
+
+  $('#SSN-mask').val(maskVal);
+  $('#SSN-mask').removeClass('hidden');
+  $('#SSN-mask').css('letter-spacing', '0.7px');
+}
+
+function ssnUnmask() {
+  $('#SSN-mask').addClass('hidden');
+  $('#SSN').removeClass('hidden');
+  $('#SSN').focus();
+}
+
+$('#SSN').focusout(function () {
+  return ssnMask();
+});
+$('#SSN-mask').focus(function () {
+  return ssnUnmask();
+});
+$('#SSN').on('input', function (e) {
+  if (e.target.value.length >= 11) {
+    ssnMask();
+    $('#birthday').focus();
+  }
+});
+
+function birthdayMask() {
+  $('#birthday').addClass('hidden');
+  var maskVal = '';
+
+  for (var i = 0; i < $('#birthday').val().length; i++) {
+    if (i === 2 || i === 5) {
+      maskVal += '/';
+    } else {
+      maskVal += '*';
+    }
+  }
+
+  $('#birthday-mask').val(maskVal);
+  $('#birthday-mask').removeClass('hidden');
+  $('#birthday-mask').css('letter-spacing', '0.7px');
+}
+
+function birthdayUnmask() {
+  $('#birthday-mask').addClass('hidden');
+  $('#birthday').removeClass('hidden');
+  $('#birthday').focus();
+}
+
+$('#birthday').focusout(function () {
+  return birthdayMask();
+});
+$('#birthday-mask').focus(function () {
+  return birthdayUnmask();
+});
+$('#birthday').on('input', function (e) {
+  if (e.target.value > 1 && e.target.value.length <= 1) {
+    e.target.value = "0".concat(e.target.value);
+  }
+
+  if (e.target.value.length >= 10) {
+    birthdayMask();
+  }
+});
+
 // By default - Set Devices to 1 onload
 $('[name="devices"]').filter('[value="1 device"]').prop('checked', true);
 
